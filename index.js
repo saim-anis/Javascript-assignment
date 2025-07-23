6a677969f2adeeb65e373f188a68433bc9fdad3e
Saim Anis <saimdesign87@gmail.com>
2025-07-22 22:59:50 -0700

    all question completed

---
+++ index.js
@@ -0,0 +1,171 @@
+//asignment 1
+
+alert("Hello World!");
+
+//asignment 2
+alert("Saim Anees");
+
+//asignment 3
+alert("3");
+
+//asignment 4
+
+var message = "My Trainer name is Ibrahim";
+
+alert(message);
+
+//asignment 5
+
+let names = "Saim";
+let fnames = " Anees";
+alert(names +  fnames)
+
+//Variables for Strings: Declaring and Using Text Data
+
+//asignment 6
+var userName = "SAIM";
+//asignment 7
+var greet = "Welcome to my home";
+alert(greet);
+//asignment 8
+
+var firstName = "Muhammad ";
+var lastName = "Ali";
+var c =  firstName +  lastName;
+alert(c);
+
+
+//asignment 9
+let Quote = "Dreams don’t work unless you do";
+alert(Quote);
+
+//asignment 10
+
+let city = "Karachi"
+alert("Welcome to " + city);
+
+//Working with Numbers and Basic Arithmetic
+
+//asignment 11
+
+let age = 10;
+
+//asignment 12
+let a = 2;
+let b = 3;
+let d = a + b;
+console.log(d);
+
+//asignment 13
+
+let e = 4;
+let f = 3;
+let g = e - f;
+console.log(g);
+
+//asignment 14
+
+let h = 4;
+let i = 5;
+j = h * i;
+ console.log(j);
+
+
+//asignment 15
+
+alert("you are 20 years old");
+
+
+
+// 4.Math Expressions: Performing Arithmetic Operation
+
+//asignment 16
+
+var x = 10;
+var y = 5;
+var ans = x + y;
+console.log(ans);
+
+//asignment 17
+
+
+var x = 10;
+var y = 5;
+var minus = x - y;
+alert(minus);
+
+//asignment 18
+
+var x = 10;
+var y = 5;
+var divide = x / y;
+alert(divide);
+
+//asignment 19
+var x = 10;
+var y = 5;
+var mod = x % y;
+console.log(mod);
+
+
+//Getting User Input and Conditional Logic
+//asignment 20
+let name = prompt("What is your name?");
+ alert("Hello " + name);
+
+ //asignment 21
+
+ let userAge = prompt("write your age");
+ console.log(userAge);
+
+ //asignment 22
+
+let num = prompt("Choose any number");
+let num2 = num * 2;
+alert(num2);
+
+//asignment 23
+let color = prompt("What is your favorite colour");
+alert("Wow! I like " + color + " too" );
+
+//asignment 24
+
+let yourcity = prompt("What is your city Name");
+alert("You are from  "  +  yourcity );
+
+//asignment 25
+ var userkiAge = prompt("Apni Age Batao");
+ if (userkiAge < 30){
+    alert("Wo'hoo You're still young man")
+ }else{
+    alert("O'hoo You're very old") 
+ }
+ 
+//asignment 26
+
+let number = prompt("Type Number");
+
+if (number % 2 == 0) {
+  alert("This is Even number");
+} else {
+  alert("This is Odd number");
+}
+
+//asignment 27
+let val1 = 89;
+let val2 = 90;
+if(val1 > val2){
+    alert(val1 + " is greater than " + val2)
+}else{
+     alert(val1 + " is smaller than " + val2)
+}
+
+//asignment 28
+
+let password = prompt("Enter Correct password 'password is 123'");
+let correctPassword = "123";
+if (password === correctPassword){
+    alert("You can go");
+}else{
+     alert("Invalid password");
+}
